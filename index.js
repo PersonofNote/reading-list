69616a7f91de26a0d73ff9dd96858afc3ead15c0
Jessica Martin <jmartin@agu.org>
2018-11-13 15:38:25 -0500

    Functional, not pretty

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const book1 = new Book("Sample","Person",211,false);
 const book2 = new Book("Sample","Person",211,false);
 const book3 = new Book("Sample","Person",211,false);
 
-let myLibrary = [book1, book2,book3];
+let books = [book1, book2,book3];
 
 
 function Book(title,author,pages,read) {
@@ -11,25 +11,37 @@ function Book(title,author,pages,read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  //Update this with a progress bar
   this.pagesRead = 0;
   this.display = document.createElement('div');
-  this.display.innerHTML = `<div>
-  		<p>${this.title}</p>
+  this.display.className = "book";
+  this.display.innerHTML = `<div> <div class="book-text">
+  		<h4>${this.title}</h4>
+  		By ${this.author}</br>
+  		${this.pages}</p>
+  		</div>
   	</div>`
 }
 
+document.getElementById('submit').addEventListener("click", function() {
+  addBookToLibrary();
+  render();
+})
+
 
 function addBookToLibrary() {
-  const book4 = new Book("New","new",211,false)
-  myLibrary.push()
+  const title = document.getElementById("form-title").value;
+  const author = document.getElementById("form-author").value; 
+  const pages = document.getElementById("form-pages").value;
+  let read = false;
+  const newBook = new Book(title, author, pages, read);
+  books.push(newBook);
 }
 
 function render() {
 var shelf = document.getElementById("bookShelf");
 //Call on load and on change
-for (var i = 0; i < myLibrary.length; i++) {
- shelf.appendChild(myLibrary[i].display);
+for (var i = 0; i < books.length; i++) {
+ shelf.appendChild(books[i].display);
 }
 }
 
