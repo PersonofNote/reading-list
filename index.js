91323cf4ab599e9ec62cfbbb44afc9358c61afb9
Jessica Martin <jmartin@agu.org>
2018-11-14 10:34:09 -0500

    Fixed book form visibility toggle, working on delete button.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Pretend books, obviously
 const book1 = new Book("Sample","Person",211,false);
-const book2 = new Book("Sample","Person",211,false);
+const book2 = new Book("Longest title ever","Person",211,false);
 const book3 = new Book("Sample","Person",211,false);
 
 let books = [book1, book2,book3];
@@ -13,11 +13,13 @@ function Book(title,author,pages,read) {
   this.read = read;
   this.pagesRead = 0;
   this.display = document.createElement('div');
-  this.display.className = "book";
+  this.display.classList.add('book');
   this.display.innerHTML = `<div> <div class="book-text">
   		<h4>${this.title}</h4>
   		By ${this.author}</br>
   		${this.pages}</p>
+  		<button>Read</button>
+  		<button class="delete-button">Delete</button>
   		</div>
   	</div>`
 }
@@ -25,8 +27,24 @@ function Book(title,author,pages,read) {
 document.getElementById('submit').addEventListener("click", function() {
   addBookToLibrary();
   render();
+  toggleVisible();
 })
 
+document.getElementById('add-new').addEventListener("click", function() {
+  	toggleVisible();
+})
+
+
+function removeBook() {
+    var element = document.getElementById(this.id);
+    console.log(this.id)
+    //element.parentNode.removeChild(element);
+}
+
+function toggleVisible() {
+    var div = document.getElementById("book-form");
+    div.style.display = div.style.display == "none" ? "block" : "none";
+}
 
 function addBookToLibrary() {
   const title = document.getElementById("form-title").value;
@@ -35,6 +53,9 @@ function addBookToLibrary() {
   let read = false;
   const newBook = new Book(title, author, pages, read);
   books.push(newBook);
+  newBook.id = books.indexOf(newBook);
+  console.log(newBook.id);
+
 }
 
 function render() {
